acd4e56d95af4a13ece2e0d3041e7f7a2a78b699
Amjad Ghoufrane <ghoufraneamjad8@gmail.com>
2025-01-09 11:20:28 +0100

    Print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from structures.arbres import *
-
+from parseur import * 
 
 def formule_vers_arbre(formule):
     formule = formule.replace(" ", "")
@@ -48,11 +48,33 @@ def formule_vers_arbre(formule):
 
     return noeud(operateur,formule_vers_arbre(gauche),formule_vers_arbre(droite))
 
-entree = input("Entrez votre calcul : ")
+
+def print_arbre(node, level=0, prefix=""):
+    if node is None:
+        return
+    
+    space = "  "
+    
+    if node.getDroite():
+        print_arbre(node.getDroite(), level + 1, "/")
+    
+    if node.getValeur() == "/":
+        print(space * level + prefix + str(node.getValeur().replace("/","÷")))
+    else :
+        print(space * level + prefix + str(node.getValeur()))
+
+    if node.getGauche():
+        print_arbre(node.getGauche(), level + 1, "\\")
+
+
+# entree = input("Entrez votre calcul : ")
+entree = latex_a_math()
 arbre2 = formule_vers_arbre(entree)
 
-print("Arbre 2")
-print(arbre2.parcoursInfixe())
+
+
+print_arbre(arbre2)
+print("\n"+str(arbre2.parcoursInfixe()))
 
 
 
